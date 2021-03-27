bccec01d2ff741da0c6745f3e0ce7af32c263b39
pranavdv <71985134+pranavdv@users.noreply.github.com>
2021-03-27 23:19:19 +0530

    updated employee table,removed comments

--- initialdesign.sql
+++ initialdesign.sql
@@ -4,8 +4,7 @@ use project;
 create table admin(
 	admin_id int primary key,
     admin_name varchar(100),
-    password varchar(100),
-    --email_id varchar(100)
+    password varchar(100)
 );
 
 create table dept(
@@ -15,7 +14,7 @@ create table dept(
 
 create table grade(
 	grade_id int primary key auto_increment,
-    grade_name varchar(100) ,
+    grade_name varchar(100),
     grade_short_name varchar(30),
     grade_basic int ,
     grade_ta int ,
@@ -24,33 +23,30 @@ create table grade(
     grade_ma int ,
     grade_bonus int,
     grade_pf int ,
-    grade_pt int,
-    -- constraint nonzero check (grade_basic>=0 and grade_ta>=0 and grade_da>=0 and grade_hra>=0 and grade_ma>=0 and grade_bonus>=0 and grade_pf>=0 and grade_pt>=0)
+    grade_pt int
 );
 
 create table employee(
-	emp_id int primary key auto_increment;
-    emp_title varchar(100) ,
-    emp_name varchar(100) ,
+	emp_id int primary key auto_increment,
+    emp_title varchar(100),
+    emp_name varchar(100),
     emp_dob date,
+    emp_address varchar(100),
     emp_city varchar(100),
-    emp_pincode int,
-    emp_mobile_number char(10);
-    emp_pancard_number char(10),
     emp_state varchar(100),
+    emp_pincode int,
+    emp_mobile_number char(10),
+    emp_pancard_number char(10),
     emp_mail_id varchar(100) unique
 );
 
 create table emp_grade(
-	emp_id int unique,
-    emp_dept_id int ,
-    emp_grade_id int ,
-    -- emp_from_date date ,
-    -- emp_to_date date ,
+	emp_id int,
+    emp_dept_id int,
+    emp_grade_id int,
     foreign key (emp_id) references employee(emp_id),
     foreign key (emp_dept_id) references dept(dept_id),
-    foreign key (emp_grade_id) references grade(grade_id),
-    constraint valid_dates check (emp_to_date > emp_from_date)
+    foreign key (emp_grade_id) references grade(grade_id)
 );
 
 create table transaction(
