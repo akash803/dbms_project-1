8d050f8cdcb9fc0201f253a1d095b9a55200fcf6
pranavdv <71985134+pranavdv@users.noreply.github.com>
2021-03-27 23:14:26 +0530

    updated app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -31,7 +31,7 @@ console.log(__dirname);
 //app.use(express.static(__dirname + '/images'));
 
 app.get('/login1.html', function(req, res) {
-  res.sendfile(__dirname + '/login1.html');
+  res.sendFile(__dirname + '/login1.html');
 }); 
 
 app.post('/login1.html', (req,res) => {
@@ -89,24 +89,31 @@ app.get('/login3.html', function(req, res) {
 app.post('/login3.html', (req,res) => {
 	console.log("NO ERROR!!");
 
-  var title,name,city,state,pincode,phone,email,dob,pancardno;
+  var title,name,city,state,pincode,phone,email,dob,pancardno,addr;
   title = req.body.employeeetitle;
   name = req.body.employeename;
   city = req.body.city;
   state = req.body.state;
   pincode = req.body.pincode;
   email = req.body.email;
-  dob = req.body.dob;
-  pancardno = req.body.pancardno;
+  dob = req.body.DOB;
+  //fontend pancardno = req.body.pancardno;
+  // frontend addr = req.body.address;
+  // frontend phone = req.body.phone;
 
   con.connect(function(err) {
 
     console.log("connecting to database");
     if(!err) console.log('connected');
 
-    con.query("INSERT INTO employee values ("+title+","+name+","+dob+","+city+","+pincode+","+phone+","+pancardno+","+state+","+email+")", function (err, result, fields) {
-      if (err) p="\0";
-      else console.log(result);
+    con.query("INSERT INTO employee values ("+title+","+name+","+dob+","+address+","+city+","+state+","+pincode+","+phone+","+pancardno+","+email+")", function (err, result, fields) {
+      if (err){
+        // what should we do?
+      }
+      else{
+        // again, do what?
+        console.log(result);
+      }
     });
   });
 });
@@ -118,7 +125,7 @@ app.get('/login4.html', function(req, res) {
 app.post('/login4.html', (req,res) => {
 	console.log("NO ERROR!!");
 
-  var dept_name = req.body.dept_name;
+  // frontend var dept_name = req.body.dept_name;
 
   con.connect(function(err) {
 
@@ -126,8 +133,13 @@ app.post('/login4.html', (req,res) => {
     if(!err) console.log('connected');
 
     con.query("INSERT INTO dept values ("+dept_name+")", function (err, result, fields) {
-      if (err) res.sendFile(path.join(__dirname+'login4.html'));
-      else console.log(result);
+      if (err){
+        // do what?
+      }
+      else{
+        // do what?
+        console.log(result);
+      }
     });
   });
 });
@@ -135,25 +147,75 @@ app.post('/login4.html', (req,res) => {
 app.get('/login5.html', function(req, res) {
   res.sendFile(__dirname + '/login5.html');
 });
-app.get('/login6.html', function(req, res) {
-  res.sendFile(__dirname + '/login6.html');
-});
-app.get('/login7.html', function(req, res) {
-  res.sendFile(__dirname + '/login7.html');
-});
 
-app.post('/login7.html', (req,res) => {
-	console.log("NO ERROR!!");
-  con.connect(function(err) {
+app.post('/login5.html',(req,res)=>{
+  console.log("NO ERROR!!");
+
+  var name = req.body.gradename;
+  var shortname = req.body.gradeshortname;
+  var basic = req.body.basic;
+  var da = req.body.dearnessallowance;
+  var ta = req.body.travelallowance;
+  var ma = req.body.medicalallowance;
+  var hra = req.body.hra;
+  var bonus = req.body.bonus;
+  var pt = req.body.professionaltax;
+  var pf = req.body.providentfund;
+
+  con.connect(function(err){
 
     console.log("connecting to database");
     if(!err) console.log('connected');
     
-    var dept_name = req.body.dept_name;
-
-    con.query("", function (err, result, fields) {
-      if (err) p="\0";
+    con.query("INSERT INTO grade values ("+name+","+shortname+","+basic+","+ta+","+da+","+hra+","+ma+","+bonus+","+pf+","+pt+")",function(err,result,fields){
+      if (err) res.sendFile(path.join(__dirname+'login5.html'));
       else console.log(result);
+    });
+  });
+});
+
+app.get('/login7.html', function(req, res) {
+  res.sendFile(__dirname + '/login7.html');
+});
+
+app.post('/login7.html',(req,res)=>{
+  console.log("NO ERROR!!");
+
+  // frontend var e_id = req.body.e_id;
+
+  con.connect(function(err){
+
+    console.log("connecting to database");
+    if(!err) console.log('connected');
+
+    con.query();
+  });
+});
+
+app.get('/login8.html', function(req, res) {
+  res.sendFile(__dirname + '/login8.html');
+});
+
+app.post('/login8.html',function(req,res){
+  console.log("NO ERROR!!");
+
+  // frontend var e_id = req.body.e_id;
+  // frontend var d_id = req.body.d_id;
+  // frontend var g_id = req.body.g_id;
+
+  con.connect(function(err){
+
+    console.log("connecting to database");
+    if(!err) console.log('connected');
+
+    con.query("INSERT INTO emp_grade values ("+e_id+","+d_id+","+g_id+")",function(err,result,fields){
+      if (err){
+        // do what?
+      }
+      else{
+        // do what?
+        console.log(result);
+      }
     });
   });
 });
